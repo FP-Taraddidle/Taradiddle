8ed3e2aeb9bd18a7b683cd37f8bde06eb890424d
SoriaMiguel <soria.miguel.8@gmail.com>
2017-04-04 11:39:21 -0400

    created sessions controller, new users can sign in and are redirected to twitter, users are stored but without email

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-  get '/auth/:provider/callback', to: 'sessions#create'
+  get   '/login', :to => 'sessions#new', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create', via: 'get'
+  match '/auth/failure', :to => 'sessions#failure', via: 'get'
 
-  
   resources :users
+
+  scope :api do
+
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
