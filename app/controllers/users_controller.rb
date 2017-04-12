ea6e3470dc11ad2d471cfcc7c8f43a02497c20dd
SoriaMiguel <soria.miguel.8@gmail.com>
2017-04-12 14:20:33 -0400

    added redirect routes in sessions controller for users login or creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,16 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  def updated
+
+  end
+
+
+  def email
+    user = User.find(params[:id])
+    @address = twitter_client.user(user.name).email
+    binding.pry
+    render json: @address
+  end
+
 end
- 
