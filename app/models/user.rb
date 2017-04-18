ba0a9753daab38e79cf8c25903e942e9fe3f0c6d
SoriaMiguel <soria.miguel.8@gmail.com>
2017-04-18 15:14:51 -0400

    added optional true to user for intensity

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :authorizations
   has_many :routines
   has_many :twitterings
-  belongs_to :intensity
+  belongs_to :intensity, optional: true
   has_secure_token
   # validates :name, :email, :presence => true
 end
