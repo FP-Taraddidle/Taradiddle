9b15098696b64d53446285a46556a7819fa9c72d
SoriaMiguel <soria.miguel.8@gmail.com>
2017-04-19 15:07:28 -0400

    Routine create changed to create routines for all users

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -5,14 +5,18 @@ class RoutinesController < ApplicationController
   end
 
   def create
-    @new_routine = Routine.new
-    @new_routine.user = current_user
-    @new_routine.save!
-    @tweets = @new_routine.user.twitterings.last.tweets
-    @likes = @new_routine.user.twitterings.last.likes
-    @block_count = @tweets + @likes
-    @product = random_blocks.sample(@block_count).each {|x| @new_routine.blocks << x}
-    render json: @new_routine
+   @user = User.all
+   @routines = @user.collect do |y|
+      @new_routine = Routine.new
+      @new_routine.user = y
+      @new_routine.save!
+      @tweets = @new_routine.user.twitterings.last.tweets
+      @likes = @new_routine.user.twitterings.last.likes
+      @block_count = @tweets + @likes
+      @product = random_blocks.sample(@block_count).each {|x| @new_routine.blocks << x}
+    end
+
+    render json: @routines
   end
 
   def show
---
+++ app/serializers/routines_serializer.rb
@@ -0,0 +1,3 @@
+class RoutinesSerializer < ActiveModel::Serializer
+  attributes :id, :user_id
+end
