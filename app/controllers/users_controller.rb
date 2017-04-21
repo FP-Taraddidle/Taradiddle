c09fab8ebbfcf21742b9ce3d0229f9fd2e24d09d
SoriaMiguel <soria.miguel.8@gmail.com>
2017-04-21 13:23:27 -0400

    removed binding.pry from user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,10 @@ class UsersController < ApplicationController
 
   def update
     @user = current_user
-    binding.pry
     if @user.update(user_params)
-      redirect_to "/"
+      redirect_to "/profile"
     else
-      redirect_to "/email"
+      redirect_to "/email?token=skfgjdhslfkdgjhslkhgsdofghdlskuthgfu"
     end
   end
 
